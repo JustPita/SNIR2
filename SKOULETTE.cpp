47a3142e21ffb59b1c875424762ad71ef36750e3
JustPita <sarritzu.peter@gmail.com>
2023-09-18 09:23:39 +0200

    18/09/2023 09:23

---
+++ SKOULETTE.cpp
@@ -0,0 +1,20 @@
+//~~~~
+// Peter SARRITZU
+// SNIR2
+// DD/MM/AAAA
+//~~~~
+#include <iostream> // bibliotheque de gestion des E/S
+#include <conio.h>  // gestion de la console (ici _getch())
+#include <string>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    
+
+
+    cin.get();
+    cin.ignore();
+    return EXIT_SUCCESS;
+}
