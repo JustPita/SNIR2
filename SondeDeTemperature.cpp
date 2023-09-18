96a771968d51ed134c6ba19bf4b0b660edfd5182
JustPita <sarritzu.peter@gmail.com>
2023-09-18 09:21:24 +0200

    oui

---
+++ SondeDeTemperature.cpp
@@ -0,0 +1,20 @@
+//~~~~
+// Peter SARRITZU
+// SNIR2
+// 18/09/2023
+//~~~~
+#include <iostream> // biblioth�que de gestion des E/S
+#include <conio.h>  // gestion de la console (ici _getch())
+#include <string>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    
+
+
+    cin.get();
+    cin.ignore();
+    return EXIT_SUCCESS;
+}
