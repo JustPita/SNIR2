f3ba6d2fc8018f02de5115558bf67887ff7f2b50
JustPita <sarritzu.peter@gmail.com>
2023-12-05 11:07:45 +0100

    05/12/2023

--- PObjet/second/TD1_Etudiant/Etudiant.cpp
+++ PObjet/second/TD1_Etudiant/Etudiant.cpp
@@ -1,6 +1,29 @@
 #include <iostream>
+#include <string>
 #include "Etudiant.h"
+#include "Personne.h"
 
-Etudiant::Etudiant(std::string leNom, std::string lePrenom, std::string laClasse) {
-    // Initialize your class members here
+Etudiant::Etudiant(std::string leNom, std::string lePrenom,std::string laClasse) : Personne(leNom, lePrenom) {
+    classe = laClasse;
+    moyenne = 0.0;
+    nombreEtudiants = 0;
+}
+
+std::string Etudiant::getClasse() {
+    std::cout<<classe<<std::endl;
+    return classe;
+}
+
+void Etudiant::setMoyenne(double laMoyenne) {
+    moyenne = laMoyenne;
+}
+
+double Etudiant::getMoyenne() {
+    std::cout <<moyenne<<std::endl;
+    return moyenne;
+}
+
+int Etudiant::getNombreEtudiants() {
+    std::cout<<nombreEtudiants<<std::endl;
+    return nombreEtudiants;
 }
--- PObjet/second/TD1_Etudiant/Etudiant.h
+++ PObjet/second/TD1_Etudiant/Etudiant.h
@@ -1,20 +1,21 @@
-#include <string>
-#include "Personne.h"
 #ifndef Etudiant
 #define Etudiant
+#include <string>
+#include "Personne.h"
 
-class Etudiant : public Personne{
-private:
-    std::string classe;
-    double moyenne;
-    int nombreEtudiants;
-
+class Etudiant : public Personne
+{
 public:
     Etudiant(std::string leNom,std::string lePrenom,std::string laClasse);
     std::string getClasse();
     void setMoyenne(double laMoyenne);
     double getMoyenne();
     int getNombreEtudiants();
+
+private:
+    std::string classe;
+    double moyenne;
+    int nombreEtudiants;
 };
 
 #endif
